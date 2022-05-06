5db429964a73fa2b7cf786c19bbf27ca955ababe
hi019 <65871571+hi019@users.noreply.github.com>
2022-05-05 21:43:41 -0400

    Fix logs command

--- pkg/cmd/logs.go
+++ pkg/cmd/logs.go
@@ -1,34 +1,38 @@
 package cmd
 
-//
-//var logsCmd = &cobra.Command{
-//	Use:   "logs",
-//	Short: "Logs",
-//	Args:  cobra.ExactArgs(1),
-//	RunE: func(cmd *cobra.Command, args []string) error {
-//		LoginGate()
-//		ctx := context.Background()
-//		client := config.GetAPIClient()
-//
-//		projectPath := args[0]
-//		project, err := client.GetProjectByPath(ctx, projectPath)
-//		if err != nil {
-//			return err
-//		}
-//
-//		logs, err := client.GetProjectLogs(ctx, project.ID.String())
-//		if err != nil {
-//			return err
-//		}
-//
-//		for _, log := range logs {
-//			fmt.Println(log)
-//		}
-//
-//		return nil
-//	},
-//}
-//
-//func init() {
-//	rootCmd.AddCommand(logsCmd)
-//}
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var logsCmd = &cobra.Command{
+	Use:   "logs",
+	Short: "Logs",
+	Args:  cobra.ExactArgs(1),
+	RunE: withCmdConfig(func(c *CmdConfig) error {
+		return checkLoginAndRun(c, Logs)
+	}),
+}
+
+func Logs(c *CmdConfig) error {
+	project, err := c.client.GetProjectByPath(c.ctx, c.args[0])
+	if err != nil {
+		return err
+	}
+
+	logs, err := c.client.GetProjectLogs(c.ctx, project.ID)
+	if err != nil {
+		return err
+	}
+
+	for _, log := range logs {
+		fmt.Print(log)
+	}
+
+	return nil
+}
+
+func init() {
+	rootCmd.AddCommand(logsCmd)
+}
