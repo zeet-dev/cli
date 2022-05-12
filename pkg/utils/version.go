1d0e7c52a2fc37b9009b019231cdacbae9d26b01
hi019 <65871571+hi019@users.noreply.github.com>
2022-05-12 13:17:02 -0400

    Add GetBuildVersion docs

--- pkg/utils/version.go
+++ pkg/utils/version.go
@@ -2,6 +2,8 @@ package utils
 
 import "runtime/debug"
 
+// GetBuildVersion returns the CLI release version, or the latest git commit shorthash if the release could not be found (for local builds)
+// Based on https://github.com/cue-lang/cue/issues/1697#issuecomment-1122097477
 func GetBuildVersion() string {
 	info, ok := debug.ReadBuildInfo()
 	if !ok {
