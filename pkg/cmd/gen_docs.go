1c6cb31c115fe86e002e5d07b1213a89f44f5923
hi019 <65871571+hi019@users.noreply.github.com>
2022-05-12 14:31:30 -0400

    Simplify genDocs, ignore hidden commands

--- pkg/cmd/gen_docs.go
+++ pkg/cmd/gen_docs.go
@@ -34,11 +34,8 @@ hide_title: true
 `
 
 func GenDocs(dir string) error {
-	for _, cmd := range rootCmd.Commands() {
-		err := writeDocs(cmd, dir)
-		if err != nil {
-			return err
-		}
+	if err := writeDocs(rootCmd, dir); err != nil {
+		return err
 	}
 
 	return nil
@@ -46,6 +43,7 @@ func GenDocs(dir string) error {
 
 func writeDocs(cmd *cobra.Command, dir string) error {
 	for _, c := range cmd.Commands() {
+		fmt.Println(c.Name())
 		if !c.IsAvailableCommand() || c.IsAdditionalHelpTopicCommand() || c.Hidden {
 			continue
 		}
